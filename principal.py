8c646666ca669cd0bbc1680be942808bd38d0570
Joao Vitor S Rezende <jvsrpassos@gmail.com>
2019-11-25 18:24:54 -0300

    Correção de nome de variáveis;

--- principal.py
+++ principal.py
@@ -1,15 +1,15 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-class DataVertex(object):
-    def __init__(self, dataVertexId, subject, schoolClass, teacher):
-        self.dataVertexId, self.subject, self.schoolClass, self.teacher = dataVertexId, subject, schoolClass, teacher
+class VertexData(object):
+    def __init__(self, vertexDataId, subject, schoolClass, teacher):
+        self.vertexDataId, self.subject, self.schoolClass, self.teacher = vertexDataId, subject, schoolClass, teacher
         self.adjacents, self.schedule_id, self.saturation = [], None, -1
 
 file = open("./public/files/dados_teste.txt")
 
 vertex = []
-listVertexId = []
+vertexListId = []
 G = nx.Graph()
 
 n = 0
@@ -17,20 +17,20 @@ for line in file:
     dados = line.split("|")
     numberClasses = int(dados[3].strip())
     for i in range(numberClasses):
-        listVertexId.append(n)
-        dataVertex = DataVertex(n, dados[0].strip(), dados[1].strip(), dados[2].strip())
+        vertexListId.append(n)
+        vertexData = VertexData(n, dados[0].strip(), dados[1].strip(), dados[2].strip())
         G.add_node(n)
-        vertex.append(dataVertex)
+        vertex.append(vertexData)
         n += 1
 
 file.close()
 
 for v1 in vertex:
     for v2 in vertex:
-        if(v1.dataVertexId != v2.dataVertexId and (v1.subject == v2.subject or v1.teacher == v2.teacher)):
+        if(v1.vertexDataId != v2.vertexDataId and (v1.subject == v2.subject or v1.teacher == v2.teacher)):
             v1.adjacents.append(v2)
             v2.adjacents.append(v1)
-            G.add_edge(v1.dataVertexId, v2.dataVertexId)
+            G.add_edge(v1.vertexDataId, v2.vertexDataId)
 
 file = open("./public/files/horarios_teste.txt")
 
@@ -48,9 +48,9 @@ for day in days_week:
 file.close()
 
 notColoring = []
-while(len(listVertexId) > 0):
-    maxVertex = vertex[listVertexId[0]]
-    for index in listVertexId:
+while(len(vertexListId) > 0):
+    maxVertex = vertex[vertexListId[0]]
+    for index in vertexListId:
         if(vertex[index].saturation > maxVertex.saturation or (vertex[index].saturation == maxVertex.saturation
             and len(vertex[index].adjacents) > len(maxVertex.adjacents))):
             maxVertex = vertex[index]
@@ -69,9 +69,9 @@ while(len(listVertexId) > 0):
             adjacent.saturation += 1
         maxVertex.schedule_id = color
     else:
-        notColoring.append(maxVertex.dataVertexId)
+        notColoring.append(maxVertex.vertexDataId)
 
-    listVertexId.remove(maxVertex.dataVertexId)
+    vertexListId.remove(maxVertex.vertexDataId)
 
 print(notColoring)
 
